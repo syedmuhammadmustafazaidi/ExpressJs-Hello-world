0796559170d3b0262bc89db230ae8f6535cd6198
Syed Muhammad Mustafa Zaidi <121306109+syedmuhammadmustafazaidi@users.noreply.github.com>
2023-08-12 05:03:22 +0500

    Add files via upload

---
+++ server.mjs
@@ -0,0 +1,25 @@
+ 
+import express from 'express'
+
+const app = express()
+
+
+//http://192.168.10.2:3001
+
+app.get('/', (req, res) => {
+  console.log('Maliha', new Date());
+  res.send('Maliha!' + new Date())
+})
+
+
+app.get('/profile', (req, res) => {
+  console.log('profile', new Date());
+  res.send('profile' + new Date())
+})
+
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Example server listening on port ${port}`)
+})
